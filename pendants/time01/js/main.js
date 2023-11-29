b2ca7aee7a3ab02d62486c2395f704eb11b60f38
wata <wang_wata@163.com>
2023-11-29 09:29:46 +0800

    时钟挂件添加星期

--- pendants/time01/js/main.js
+++ pendants/time01/js/main.js
@@ -5,6 +5,8 @@ setInterval(() => {
     const time = new Date();
     const year = time.getFullYear();
     const month = time.getMonth() + 1;
+    var l = ["日","一","二","三","四","五","六"];
+    var d = time.getDay();
     const day = time.getDate();
     const secs = time.getSeconds() * 6;
     const mins = time.getMinutes() * 6;
@@ -12,7 +14,7 @@ setInterval(() => {
     sec.style.transform = `rotateZ(${secs}deg)`;
     min.style.transform = `rotateZ(${mins}deg)`;
     hr.style.transform = `rotateZ(${hrs + (mins / 12)}deg)`;
-    document.querySelector('.date').textContent = [year.toString().padStart(2, 0), month.toString().padStart(2, 0), day.toString().padStart(2, 0)].join('.');
+    document.querySelector('.date').textContent = [year.toString().padStart(2, 0), month.toString().padStart(2, 0), day.toString().padStart(2, 0)].join('.') + ' 星期' + l[d].toString();
     document.querySelector('.time').textContent = `${time.getHours().toString().padStart(2, 0)}:${time.getMinutes().toString().padStart(2, 0)}:${time.getSeconds().toString().padStart(2, 0)}`;
 })
 document.onkeydown = (e) => {
